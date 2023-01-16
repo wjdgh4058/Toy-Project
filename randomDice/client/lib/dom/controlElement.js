187268a63e4cd96953399f96d685e9f18c7e2dff
wjdgh4058 <ljhmoa@gmail.com>
2023-01-16 11:51:17 +0900

    [추가] controlElement 적용

--- randomDice/client/lib/dom/controlElement.js
+++ randomDice/client/lib/dom/controlElement.js
@@ -1,15 +1,16 @@
 import { typeError } from "../error/typeError.js";
+import { isElement } from "../utils/typeOf.js";
 
 export function disableElement(node) {
-  if (node.nodeType !== document.ELEMENT_NODE) {
+  if (!isElement(node)) {
     typeError("disableElement 함수의 인자는 DOM 요소 노드 이어야 합니다.");
   }
-  node.disable = true;
+  node.disabled = true;
 }
 
-export function ensableElement(node) {
-  if (node.nodeType !== document.ELEMENT_NODE) {
+export function enableElement(node) {
+  if (!isElement(node)) {
     typeError("disableElement 함수의 인자는 DOM 요소 노드 이어야 합니다.");
   }
-  node.disable = false;
+  node.disabled = false;
 }
--- randomDice/client/main.js
+++ randomDice/client/main.js
@@ -1,16 +1,32 @@
-import { diceAnimation, getNode } from "./lib/index.js";
+import {
+  diceAnimation,
+  disableElement,
+  enableElement,
+  getNode,
+  getNodes,
+} from "./lib/index.js";
 
-const rollingDiceButton = getNode(".buttonGroup > button:nth-child(1)");
+// const rollingDiceButton = getNode(".buttonGroup > button:nth-child(1)");
+// const recordButton = getNode(".buttonGroup > button:nth-child(2)");
+const [rollingDiceButton, recordButton, resetButton] = getNodes(
+  ".buttonGroup > button"
+);
 
 const handlerRollingDice = (() => {
-  let stopAnimation;
   let isRolling = false;
+  let stopAnimation;
 
   return () => {
     if (!isRolling) {
       stopAnimation = setInterval(diceAnimation, 100);
+      // recordButton.disabled = true;
+      disableElement(recordButton);
+      disableElement(resetButton);
     } else {
       clearInterval(stopAnimation);
+      // recordButton.disabled = false;
+      enableElement(recordButton);
+      enableElement(resetButton);
     }
     isRolling = !isRolling;
   };
