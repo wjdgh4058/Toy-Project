f36153d69b149c69190ccf3c2ec1e1586a16a2b2
wjdgh4058 <ljhmoa@gmail.com>
2023-01-16 23:45:26 +0900

    [추가] memo lib

---
+++ randomDice/client/lib/utils/memo.js
@@ -0,0 +1,24 @@
+export const memo = (() => {
+  const cache = {};
+  return (key, callback) => {
+    if (!callback) return cache[key];
+
+    if (cache[key]) {
+      console.warn(`${key} 값은 이미 캐시된 값이 존재합니다`);
+      return;
+    }
+
+    cache[key] = callback();
+    console.log(cache);
+  };
+})();
+/* 
+memo("cube", () => {
+  return document.querySelector("#cube");
+});
+
+console.log(memo("cube")); */
+
+// cache = {
+//   name: 'tiget',
+// }
