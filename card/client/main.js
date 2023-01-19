428000ae6a48dd75d1e95214badcbc545d9cad4d
wjdgh4058 <ljhmoa@gmail.com>
2023-01-19 14:36:28 +0900

    [추가] delet 통신

--- card/client/main.js
+++ card/client/main.js
@@ -1,26 +1,76 @@
+/* global gsap */
+
 import {
-  createUserCard,
-  getNode,
-  insertLast,
-  renderUserCard,
+  attr,
   tiger,
-  xhrData,
+  delayP,
+  insertLast,
+  changeColor,
+  getNode as $,
+  renderSpinner,
+  renderUserCard,
+  renderEmptyCard,
 } from "./lib/index.js";
 
-const userCardContainer = getNode(".user-card-inner");
+// rendingUserList 함수 만들기
+// ajax (tiger) get user List
 
-const rendingUserList = async () => {
-  let response = await tiger.get(
-    "https://jsonplaceholder.typicode.com/users/1"
-  );
+// 유저 카드 생성
+// 생성된 카드로 랜더링
 
-  let userData = response.data;
+//  1. userList.js로 갑니다.
+//  2. renderUserCard 함수를 만들기
+//  3. 만들어진 함수 안에 createUserCard를 던지고,
+//  4. renderUserCard함수를 사용했을 때  랜더링이 잘 될 수 있도록.
 
-  console.log(userData);
+const userCardContainer = $(".user-card-inner");
 
-  let newCard = createUserCard(userData);
+async function rendingUserList() {
+  renderSpinner(userCardContainer);
 
-  renderUserCard(userCardContainer, newCard);
-};
+  try {
+    await delayP(2000);
+
+    $(".loadingSpinner").remove();
+
+    let response = await tiger.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    let userData = response.data;
+    // userData.forEach(data=> renderUserCard(userCardContainer,data))
+    userData.forEach((data) => {
+      renderUserCard(userCardContainer, data);
+    });
+
+    changeColor(".user-card");
+
+    gsap.to(gsap.utils.toArray(".user-card"), {
+      x: 0,
+      opacity: 1,
+      duration: 1.5,
+      stagger: 0.2,
+    });
+  } catch (err) {
+    // console.log(err);
+    renderEmptyCard(userCardContainer);
+  }
+}
 
 rendingUserList();
+
+// 삭제 버튼을 클릭하면 콘솔창에 '삭제' 글자가 출력이 될 수 있도록 만들어 주세요.
+
+function handler(e) {
+  let deleteButton = e.target.closest("button");
+  let article = e.target.closest("article");
+
+  if (!deleteButton || !article) return; // 버튼이 아니면 실행 안함
+  // if(!article) return; // 누른 대상의 인접한 대상이 article이 아니면 실행 안함.
+
+  let id = attr(article, "data-index").slice(5);
+
+  tiger.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+}
+
+userCardContainer.addEventListener("click", handler);
